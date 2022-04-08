8f9647e1f3ba43c78363040b69d611d54120737c
Oluwatosin <ajanaku.similoluwa@gmail.com>
2022-04-08 14:05:16 +0000

    create exercise file 1

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * n: variable
+ * srand: produces a random number
+ * each time the code is run.
+ *
+ *if-else: control structure
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>0)
+    {
+        printf("%d is positive\n", n);
+    }
+    
+    else if (n==0) 
+    {
+        printf("%d is zero\n", n);
+    }
+    else 
+    {
+        printf("%d is negative\n", n);
+    }
+	return (0);
+}
